b81245d552816e8499b2a656450222b1793eff26
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-28 16:49:20 -0700

    still building components

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ import ReactDOM from "react-dom";
 class TimerInput extends React.Component {
     render() {
         return (
-            <div>
+            <div style={{marginLeft:100}}>
                 <h3>Input your desired time</h3>
                 <input type="number" required />
             </div>
@@ -19,7 +19,7 @@ class Timer extends React.Component {
     render() {
         return (
             <div>
-                <h1> </h1>
+                <h1 style={{ fontSize: 100, marginLeft:100 }}> </h1>
             </div>
         );
     }
@@ -28,9 +28,31 @@ class Timer extends React.Component {
 class StartButton extends React.Component {
     render() {
         return (
-            <div>
+            <div style={{ marginLeft: 130 }}>
                 <button>Start</button>
             </div>
         );
+    }
+}
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <TimerInput />
+                <Timer />
+                <StartButton />
+            </div>
+        );
+    }
+} 
+
+class App extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            seconds: '00', // responsible for the seconds
+            minutes: '' // responsible for the minutes
+        }
     }
 }
