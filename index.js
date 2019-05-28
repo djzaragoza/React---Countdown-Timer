20f5004a554724c8ec3c8e0a05cd030b53883a27
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-28 16:43:37 -0700

    still building components

--- index.js
+++ index.js
@@ -11,6 +11,26 @@ class TimerInput extends React.Component {
                 <h3>Input your desired time</h3>
                 <input type="number" required />
             </div>
-        )
+        );
+    }
+}
+
+class Timer extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1> </h1>
+            </div>
+        );
+    }
+}
+
+class StartButton extends React.Component {
+    render() {
+        return (
+            <div>
+                <button>Start</button>
+            </div>
+        );
     }
 }
