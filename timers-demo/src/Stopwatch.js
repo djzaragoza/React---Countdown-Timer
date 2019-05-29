cc2332a57a209ff754e0754bbf4823bb3909d6ab
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-28 23:00:20 -0700

    working on creating the functionality of the stopwatch.js

--- timers-demo/src/Stopwatch.js
+++ timers-demo/src/Stopwatch.js
@@ -2,6 +2,20 @@ import React, { Component } from "react";
 import "./App.css";
 
 class Stopwatch extends Component {
+    state = {
+        timerOn: false,
+        timerStart: 0,
+        timerTime: 0
+    };
+
+    startTimer = () => {
+        this.setState ({
+            timerOn: true,
+            timerTime: this.state.timerTime,
+            timerStart: Date.now() - this.state.timerTime
+        });
+    }
+
     render() {
         return (
             <div className="Stopwatch">
